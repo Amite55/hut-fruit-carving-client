af5f3f3cf8cd474bc9bb5731897b203dd4d3feb7
Amite55 <amite403@gmail.com>
2024-05-08 19:37:03 +0600

    add a new item

--- src/Pages/AddCraft/AddCraft.jsx
+++ src/Pages/AddCraft/AddCraft.jsx
@@ -1,4 +1,4 @@
-
+import Swal from 'sweetalert2'
 
 const AddCraft = () => {
 
@@ -11,8 +11,29 @@ const AddCraft = () => {
         const time = form.time.value;
         const image = form.image.value;
         const description = form.description.value;
-        const allData = {itemName, subcategory, price, time, image, description}
-        console.log(allData)
+        const newCraft = {itemName, subcategory, price, time, image, description}
+        console.log(newCraft)
+
+        // send to the server
+        fetch('http://localhost:5000/crafts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCraft)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                Swal.fire({
+                    title: 'success!',
+                    text: 'Added a New craft Item',
+                    icon: 'successful',
+                    confirmButtonText: 'ok'
+                  })
+            }
+        })
     }
 
     return (
@@ -85,3 +106,15 @@ const AddCraft = () => {
 };
 
 export default AddCraft;
+
+
+
+// https://i.ibb.co/vk68scN/Adobe-Stock-765491568-Preview.jpg
+// https://i.ibb.co/s9g8Bfz/Adobe-Stock-68622726-Preview.jpg
+// https://i.ibb.co/q5Dzs0R/Adobe-Stock-61463905-Preview.jpg
+// https://i.ibb.co/XfQrcB1/Adobe-Stock-685936811-Preview.jpg
+// https://i.ibb.co/frnjdgs/24846.jpg
+// https://i.ibb.co/xYv9FTD/10314.jpg
+// https://i.ibb.co/5KXtVWb/76103.jpg
+// https://i.ibb.co/sqCnB5m/19594.jpg
+// https://i.ibb.co/LRg4Tvj/aneta-pawlik-3-APomrmclj4-unsplash.jpg
