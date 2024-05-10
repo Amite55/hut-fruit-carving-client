6a7b195d589d9246be33998ea57af06638794e85
Amite55 <amite403@gmail.com>
2024-05-10 11:27:17 +0600

    card has been deleted

--- src/Pages/SingIn/SingIn.jsx
+++ src/Pages/SingIn/SingIn.jsx
@@ -28,7 +28,7 @@ const SingIn = () => {
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="password" placeholder="password" className="input input-bordered" required />
+              <input type="password" placeholder="*********" className="input input-bordered" required />
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
               </label>
--- src/Pages/SingUp/SignUp.jsx
+++ src/Pages/SingUp/SignUp.jsx
@@ -4,10 +4,23 @@ import { Link } from "react-router-dom";
 const SignUp = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
-
-  
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+    <div className="text-center">
+            <h1 className="text-3xl font-bold">Create Account</h1>
+          </div>
       <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="text" placeholder="Your Name" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Image</span>
+          </label>
+          <input type="url" placeholder="Photo Url" className="input input-bordered" required />
+        </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
@@ -18,7 +31,7 @@ const SignUp = () => {
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" placeholder="*********" className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
